a63c53b6b54c086fb1834aee0ff9d52346f2cbd7
Nick <znhihgiasy@gmail.com>
2020-01-29 22:59:47 +0800

    one rcon connection per request

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,25 @@
 module Main (main) where
 
-import Network.Wai.Handler.Warp (run)
-import System.Environment (getArgs)
-import qualified Data.ByteString.Lazy.UTF8 as BSU
-import System.IO (Handle, IOMode(ReadWriteMode))
+import Control.Exception (throwIO)
 import Network.Socket (socket, Family(AF_INET), SocketType(Stream), defaultProtocol, connect, SockAddr(SockAddrInet), tupleToHostAddress, socketToHandle)
-import Rcon (auth)
+import Network.Wai.Handler.Warp (run)
+import qualified Data.ByteString.Lazy.UTF8 as BSU
+import System.Environment (getArgs)
+import System.IO.Error (userError)
+import System.IO (Handle, IOMode(ReadWriteMode))
+
+import Rcon (auth, sendCommand)
 import WebService (app)
 
 main = do
     [port, password] <- getArgs
-    sock <- socket AF_INET Stream defaultProtocol
-    connect sock (SockAddrInet (read port) (tupleToHostAddress (127, 0, 0, 1)))
-    handle <- socketToHandle sock ReadWriteMode
-    success <- auth handle (BSU.fromString password)
-    if success
-        then app handle >>= run 8081
-        else putStrLn "Authentication failed."
+    app (rcon port password) >>= run 8081
+    where
+    rcon port password cmd = do
+        sock <- socket AF_INET Stream defaultProtocol
+        connect sock (SockAddrInet (read port) (tupleToHostAddress (127, 0, 0, 1)))
+        handle <- socketToHandle sock ReadWriteMode
+        success <- auth handle (BSU.fromString password)
+        if success
+            then sendCommand handle cmd
+            else throwIO (userError "Authentication failed")
--- src/WebService.hs
+++ src/WebService.hs
@@ -6,16 +6,13 @@ import Control.Monad.IO.Class (liftIO)
 import Network.Wai (Application)
 import System.IO (Handle)
 import Web.Scotty (ScottyM, scottyApp, get, raw, text, setHeader)
+import Data.ByteString.Lazy (ByteString)
 
-import Rcon (sendCommand)
-
-app :: Handle -> IO Application
+app :: (ByteString -> IO ByteString) -> IO Application
 app rcon = scottyApp do
     get "/update" do
         setHeader "Content-Type" "application/json; charset=utf-8"
-        liftIO (sendCommand rcon "/_midymidyws get_update")
-            >>= raw
+        liftIO (rcon "/_midymidyws get_update") >>= raw
     get "/players" do
         setHeader "Content-Type" "application/json; charset=utf-8"
-        liftIO (sendCommand rcon "/_midymidyws get_players")
-            >>= raw
+        liftIO (rcon "/_midymidyws get_players") >>= raw
