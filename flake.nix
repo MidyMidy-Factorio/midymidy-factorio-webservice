e58610d2e860c60f83578c18c0406964d3f4923a
Nick Ballard <znhihgiasy@gmail.com>
2024-01-20 01:25:23 +0800

    remove certain things and added stuff

--- flake.nix
+++ flake.nix
@@ -2,19 +2,22 @@
   description = "Factorio Telegram Bot";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
+    let overlay = self: super: {
+      midymidy-factorio-webservice = self.callPackage ./default.nix {};
+    }; in
     flake-utils.lib.eachDefaultSystem (system: rec {
-      packages = rec {
-        midymidy-factorio-webservice = nixpkgs.legacyPackages."${system}".callPackage ./default.nix {};
-        default = midymidy-factorio-webservice;
-      };
-      overlays.default = self: super: {
-        midymidy-factorio-webservice = self.callPackage ./default.nix {};
-      };
+      packages =
+        let pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; }; in
+        builtins.intersectAttrs (overlay 42 42) pkgs // {
+          default = packages.midymidy-factorio-webservice;
+        };
       devShells.default =
         with nixpkgs.legacyPackages.${system};
         mkShell {
           packages = [ haskell-language-server ];
           inputsFrom = [ packages.default ];
         };
-    });
+    }) // {
+      overlays.default = overlay;
+    };
 }
