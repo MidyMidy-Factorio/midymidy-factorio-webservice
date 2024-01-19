b25420b4b87439adda777f306c7e4d5efeb434aa
Nick Ballard <znhihgiasy@gmail.com>
2024-01-20 00:41:07 +0800

    I must have been drunk.

--- flake.nix
+++ flake.nix
@@ -4,29 +4,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: rec {
       packages = rec {
-        midymidy-factorio-webservice =
-          with nixpkgs.legacyPackages."${system}";
-          stdenv.mkDerivation {
-            pname = "midymidy-factorio-webservice";
-            version = "0.1.0";
-            src = ./.;
-            buildInputs = [ (haskellPackages.ghcWithPackages (p: with p; [
-              scotty wai warp text bytestring
-              utf8-string binary aeson network
-              lens lens-aeson http-conduit
-            ]) ) ];
-            buildPhase = ''
-              runHook preBuild
-              ghc -O2 ./src/*.hs -o midymidy-factorio-webservice
-              runHook postBuild
-            '';
-            installPhase = ''
-              runHook preInstall
-              mkdir -p $out/bin
-              cp ./midymidy-factorio-webservice $out/bin/
-              runHook postInstall
-            '';
-          };
+        midymidy-factorio-webservice = nixpkgs.legacyPackages."${system}".callPackage ./default.nix {};
         default = midymidy-factorio-webservice;
       };
       devShells.default =
